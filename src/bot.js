9d1a1e52edcb44e4042d40bee6d22b5d70a21f10
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-14 16:59:02 -0500

    Remove Winston logger
    https://www.npmjs.com/package/winston

--- src/bot.js
+++ src/bot.js
@@ -1,19 +1,10 @@
-var logger = require('winston');
 var auth = require('./auth.json');
 var Discord = require('discord.js');
 const config = require("./auth.json");
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-      colorize: true
-});
-logger.level = 'debug';
-
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  logger.info('Connected');
   console.log('Logged in as ${client.user.tag}!');
 });
 
