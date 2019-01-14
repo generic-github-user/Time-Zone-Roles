1290519c9b8a5b87fed1ecf2e1d9c3ce1d8e98ac
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-14 18:52:42 -0500

    Add basic settings for discord.js Commando module
    As described in https://github.com/discordjs/Commando-guide/blob/master/getting-started.md

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,31 @@
 var auth = require('./auth.json');
 var Discord = require('discord.js');
 var Commando = require('discord.js-commando');
+const path = require('path');
 
-const client = new Commando.Client();
+const client = new Commando.Client({
+	commandPrefix: '?',
+    owner: auth.owner,
+    disableEveryone: true
+});
 
 var settings = {
 	"prefix": "?"
 };
 s = settings;
 
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['Command Group']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('with time');
 });
 
 client.on('message', msg => {
