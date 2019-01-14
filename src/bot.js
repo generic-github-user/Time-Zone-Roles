66d27b03b281d573dd1fa24383f7514dba7aad8c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-14 16:48:34 -0500

    Update code to use discord.js
    https://discord.js.org/#/

--- src/bot.js
+++ src/bot.js
@@ -1,39 +1,27 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var DiscordJS = require('discord.js');
+const config = require("./auth.json");
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
       colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-      token: auth.token,
-      autorun: true
-});
-bot.on('ready', function(evt) {
-      logger.info('Connected');
-      logger.info('Logged in as: ');
-      logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function(user, userID, channelID, message, evt) {
-      // Our bot needs to know if it will execute a command
-      // It will listen for messages that will start with `!`
-      if (message.substring(0, 1) == '!') {
-            var args = message.substring(1).split(' ');
-            var cmd = args[0];
 
-            args = args.splice(1);
-            switch (cmd) {
-                  // !ping
-                  case 'ping':
-                        bot.sendMessage({
-                              to: channelID,
-                              message: 'Ponnnng!'
-                        });
-                        break;
-                        // Just add any case commands if you want to..
-            }
-      }
+const client = new DiscordJS.Client();
+
+client.on('ready', () => {
+  logger.info('Connected');
+  console.log('Logged in as ${client.user.tag}!');
 });
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(config.token);
