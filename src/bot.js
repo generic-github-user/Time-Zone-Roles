3f11f7759979c776017bfea713210773f4ac0149
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-14 17:48:18 -0500

    Add settings object and command message prefix

--- src/bot.js
+++ src/bot.js
@@ -3,12 +3,17 @@ var Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+var settings = {
+	"prefix": "?"
+};
+s = settings;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === s.prefix + 'ping') {
     msg.reply('Pong!');
   }
 });
