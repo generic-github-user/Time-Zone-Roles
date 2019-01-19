1b5ddfaad597488e13867051d45edb3ccc006f3c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-01-19 10:23:17 -0500

    List existing timezone roles (as well as all timezone names) when zone command is used
    #22

--- src/commands/main/zones.js
+++ src/commands/main/zones.js
@@ -16,11 +16,28 @@ module.exports = class SayCommand extends Command {
 				usages: 1,
 				duration: 15
 			}
-        });    
+        });
     }
 
     run(msg) {
 		msg.say("Available timezones:");
 		msg.say(settings.timezones.join(", "));
+		
+		var roles = msg.guild.roles.filter(
+			x => containsZones(toRoleName(x.name))
+		);
+		if (roles.size > 0) {
+			msg.say('Existing timezone roles on this server:');
+			msg.say(
+				roles.map(
+					y => containedZones(
+						settings.timezones,
+						y.name
+					)[0]
+				).join(', ')
+			);
+		} else {
+			msg.say('There are no timezone roles on this server - use `?setzone <timezone>` to add a timezone to a user.');
+		}
     }
 };
